16fcd39b9d51bf5e6cbe9d1aac3cbd22f810e2fa
Viral Patel <pviral810@gmail.com>
2018-07-23 14:54:56 -0400

    Show top 5 posts on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,34 @@
   <div class="home">
     <img src="../assets/logo.png">
     <h1>Welcome to Your Vue.js powered Wordpress frontend App</h1>
+    <hr />
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h2>{{ post.title.rendered }}</h2>
+        <p v-html="post.excerpt.rendered"></p>
+      </li>
+    </ul>
   </div>
 </template>
-
+<style>
+ul{
+  list-style-type: none;
+  text-align: left;
+  margin: 30px 0;
+  padding: 0;
+}
+</style>
 <script>
 // @ is an alias to /src
+import api from '@/api';
 
 export default {
   name: 'home',
-  components: {
+  data: () => ({
+    posts: []
+  }),
+  beforeCreate() {
+    api.getPosts( null, posts => this.posts = posts)
   }
 }
 </script>
